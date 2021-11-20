e89350536a35dd1b6671e7567bb9ef1ba3945232
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-11-20 20:03:26 +0800

    封装网络请求

---
+++ request/index.js
@@ -0,0 +1,29 @@
+let ajaxTimec = 0
+export const request = (params) => {
+  ajaxTimec++
+  wx.showLoading({
+    title: '加载中...',
+    mask: true
+  })
+  const baseURL = "https://api-hmugo-web.itheima.net/api/public/v1"
+  return new Promise((resolve, reject) => {
+    wx.request({
+      ...params,
+      url: baseURL + params.url,
+      success: (result) => {
+
+        resolve(result)
+      }, 
+      fail: (err) => {
+        reject(err)
+      },
+      complete: () => {
+        ajaxTimec--
+        if (ajaxTimec===0) {
+          wx.hideLoading()
+        }
+
+      }
+    })
+  })
+}
